3dbb1a5ce2827d715d3ee46b1b320fb861c89397
ruanshudong <ruanshudong@qq.com>
2020-03-21 10:51:51 +0800

    add echo

--- autorun.sh
+++ autorun.sh
@@ -6,18 +6,24 @@ HOSTIP=`ifconfig | sed 's/addr//g' | grep eth0 -A3 | grep "inet " | awk -F'[ :]+
 
 echo HOSTIP:${HOSTIP}
 
+echo "docker pull mysql"
 docker pull mysql:5.6
 
+echo "docker run mysql"
 docker run -dit -p 3306:3306 -e MYSQL_ROOT_PASSWORD=12345 mysql:5.6 
 
+echo "docker pull tars framework"
 docker pull tarscloud/framework
 
+echo "docker run tars framework"
 docker run --net=host -e MYSQL_HOST=${HOSTIP} -e MYSQL_ROOT_PASSWORD=12345 \
         -eREBUILD=true -eINET=eth0 -eSLAVE=false \
         -v/data/tars:/data/tars \
         -v/etc/localtime:/etc/localtime \
         tarscloud/framework
 
-./build-docker.sh		
+echo "docker build tars-demo"
+docker build docker -t tars-demo
 
+echo "docker run tars-demo"
 docker run -it -eWEB_HOST=${HOSTIP} -eMYSQL_HOST=${HOSTIP} -eTARS_TOKEN=xxxx tars-demo 
