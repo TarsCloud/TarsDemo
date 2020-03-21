af9d7997705391705059650f743c42acb07d017c
ruanshudong <ruanshudong@qq.com>
2020-03-21 10:47:38 +0800

    add autorun.sh

---
+++ autorun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ifconfig
+
+HOSTIP=`ifconfig | sed 's/addr//g' | grep eth0 -A3 | grep "inet " | awk -F'[ :]+' '{print $3}'`
+
+docker pull mysql:5.6
+
+docker run -dit -p 3306:3306 -e MYSQL_ROOT_PASSWORD=12345 mysql:5.6 
+
+docker pull tarscloud/framework
+
+docker run --net=host -e MYSQL_HOST=${HOSTIP} -e MYSQL_ROOT_PASSWORD=12345 \
+        -eREBUILD=true -eINET=eth0 -eSLAVE=false \
+        -v/data/tars:/data/tars \
+        -v/etc/localtime:/etc/localtime \
+        tarscloud/framework
+
+./build-docker.sh		
+
+docker run -it -eWEB_HOST=${HOSTIP} -eMYSQL_HOST=${HOSTIP} -eTARS_TOKEN=xxxx tars-demo 
